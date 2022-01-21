c10ed6a48894b9e07ca3463b0c496872d67badd5
Alvin "Arc" Aguila Sosangyo <31480031+athurion@users.noreply.github.com>
2022-01-21 13:15:48 +0900

    Add files via upload

---
+++ ContentViewExplicitAnimation.swift
@@ -0,0 +1,39 @@
+//
+//  ContentView.swift
+//  SwiftUI Tutorial: Basic Animations
+//
+//  Created by Alvin Sosangyo on 01/21/22.
+//
+
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var heartChange = false
+    
+    var body: some View {
+        
+        ZStack {
+            
+            Image(systemName: heartChange ? "heart.fill" : "heart")
+                .font(.system(size: 100))
+                .foregroundColor(heartChange ? .red : .black)
+                .scaleEffect(heartChange ? 1.5 : 1.0)
+            
+        } //ZStack
+        .onTapGesture {
+            withAnimation(.default) {
+                self.heartChange.toggle()
+            }
+        }
+        
+    } //body
+    
+} //ContentView
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
